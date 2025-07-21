31b31634ebe5b082c687d85e68c37d46aa893e12
Benedikt Weyer <bw.development@pm.me>
2025-07-21 17:38:50 +0200

    fix: simplify initContent in zsh configuration by removing unused environment variables

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -48,16 +48,8 @@
         ls = "lsd -a";
       };
 
-      initContent = with pkgs-unstable; ''
-        export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
-        export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-        export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-        export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-        export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-
+      initContent = '
         export EDITOR="nvim"
-
-        export RCLONE_CONFIG="${config.sops.secrets."rclone_config".path}"
       '';
     };
     
